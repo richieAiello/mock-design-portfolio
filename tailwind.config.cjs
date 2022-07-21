89b727ca66c8b002816abeb6cebd7738c1364fe0
Richard Aiello <richaiello17@gmail.com>
2022-07-21 09:50:35 -0400

    Starting files and styles.

---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  corePlugins: {
+    container: false,
+  },
+  theme: {
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '1440px',
+    },
+    extend: {
+      colors: {
+        blue: '#755cde',
+        orange: '#f6a560',
+        pink: '#f39e9e',
+        peach: '#eb7565',
+        green: '#61c4b7',
+        purple: '#552049',
+        black: '#030303',
+        grey: '#7a746e',
+        white: '#fff7f0',
+      },
+    },
+  },
+  plugins: [],
+};
