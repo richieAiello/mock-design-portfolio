8e23f22b913183183702dd48ff1b1cd0d0bb0f35
Richard Aiello <richaiello17@gmail.com>
2022-07-21 10:40:31 -0400

    Styles and JSX for Header.jsx on mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
+import Header from './components/header/Header';
+
 const App = props => {
-  return (
-    <h1 className="text-black text-[2.25rem] leading-[2.835rem] font-bold">
-      Working
-    </h1>
-  );
+  return <Header />;
 };
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/logo.svg';
+
+const Header = props => {
+  return (
+    <header className="container flex justify-between items-center mt-4">
+      <img src={logo} alt="" />
+      <a
+        href="#"
+        className="text-white bg-black rounded-full font-bold py-2 px-7
+       text-[0.875rem] leading-[1.75rem] inline-block duration-[400ms]
+       cursor-pointer hover:bg-blue focus:bg-blue"
+      >
+        Free Consultation
+      </a>
+    </header>
+  );
+};
+
+export default Header;
