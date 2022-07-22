97427a001b399b8b1bec53a7c2f0268e580ea222
Richard Aiello <richaiello17@gmail.com>
2022-07-22 14:44:56 -0400

    Styles for About.jsx and children components on tablet.

--- src/components/about/AboutImage.jsx
+++ src/components/about/AboutImage.jsx
@@ -3,7 +3,8 @@ const AboutImage = props => {
     <img
       src="./image-amy.webp"
       alt="Photo of Designer Amy"
-      className="h-[300px] w-[300px] mx-auto mb-6 object-cover"
+      className="h-[300px] w-[300px] mx-auto mb-6 object-cover
+      md:absolute md:h-[364px] md:w-[364px] md:right-[408px]"
     />
   );
 };
--- src/components/shared/SharedLink.jsx
+++ src/components/shared/SharedLink.jsx
@@ -5,8 +5,9 @@ const SharedLink = ({ light, dark }) => {
     <a
       href="#"
       className={`capitalize font-bold text-[0.875rem] leading-7
-        text-cream rounded-full py-2 px-7 inline-block
-        duration-[400ms] md:text-[1rem] ${clsx({
+        text-cream rounded-full py-2 px-7 inline-block duration-[400ms]
+        md:text-[1rem] md:py-[0.875rem] md:px-[2.8125rem]
+        ${clsx({
           'bg-black': dark,
           'bg-peach': light,
           'hover:bg-blue': dark,
