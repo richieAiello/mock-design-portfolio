2e1da9f0b3cce5a2cdf583becc95aa9442688968
Richard Aiello <richaiello17@gmail.com>
2022-07-22 15:17:39 -0400

    Styles for About.jsx and children components on desktop.

--- src/components/about/AboutImage.jsx
+++ src/components/about/AboutImage.jsx
@@ -4,7 +4,8 @@ const AboutImage = props => {
       src="./image-amy.webp"
       alt="Photo of Designer Amy"
       className="h-[300px] w-[300px] mx-auto mb-6 object-cover
-      md:absolute md:h-[364px] md:w-[364px] md:right-[408px]"
+      md:absolute md:h-[364px] md:w-[364px] md:right-[408px]
+      lg:static lg:h-[445px] lg:w-[445px] lg:m-0"
     />
   );
 };
