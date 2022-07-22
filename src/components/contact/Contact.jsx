4d635224dcf515399ea5f66ee75a838c72916d5e
Richard Aiello <richaiello17@gmail.com>
2022-07-22 14:54:13 -0400

    Styles for Contact.jsx and Footer.jsx on tablet.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,11 +2,18 @@ import SharedLink from '../shared/SharedLink';
 
 const Contact = props => {
   return (
-    <section className="container py-12 px-6 bg-black text-cream text-center rounded-[0.625rem] mb-10">
-      <h4 className="font-bold text-[1.625rem] leading-[2.0475rem] mb-6">
+    <section
+      className="container py-12 px-6 bg-black text-cream 
+      text-center rounded-[0.625rem] mb-10
+      md:pt-14 md:pb-16 md:px-[4.6875rem] md:mb-14"
+    >
+      <h4
+        className="font-bold text-[1.625rem] leading-[2.0475rem] mb-6
+        md:text-[2rem] md:leading-[2.52rem] md:mb-[1.625rem]"
+      >
         Book a call with me
       </h4>
-      <p className="mb-6 max-w-[540px] mx-auto">
+      <p className="mb-6 max-w-[540px] mx-auto md:mb-[1.625rem]">
         I’d love to have a chat to see how I can help you. The best
         first step is for us to discuss your project during a free
         consultation. Then we can move forward from there.
