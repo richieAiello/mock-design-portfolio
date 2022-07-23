829bdb39d3b28d851f845a4d38e08c0ced9df875
Richard Aiello <richaiello17@gmail.com>
2022-07-23 14:30:00 -0400

    Removed lazy loading from carousel images.

--- src/components/projects/CarouselSlide.jsx
+++ src/components/projects/CarouselSlide.jsx
@@ -6,7 +6,6 @@ const CarouselSlide = props => {
       <img
         src={props.src}
         alt=""
-        loading="lazy"
         className={`
           absolute top-0 bottom-0 my-auto rounded-[0.625rem]  
           duration-[400ms]
